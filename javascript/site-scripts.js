dbb5127e4e94bc644f6f6372ec90e0901f327961
Oliver Souter <73543366+o-souter@users.noreply.github.com>
2023-03-17 17:39:27 +0000

    added more site responsivity

--- javascript/site-scripts.js
+++ javascript/site-scripts.js
@@ -6,26 +6,14 @@ const navbarcontainer = document.querySelector("nav")
 const links = document.querySelector("nav-link")
 const body = document.getElementById("body")
 
-
 hamburgermenu.addEventListener("click", toggleMenu())
 document.addEventListener("click", hideMenu())
-
-// function showMenu() {
-//     navbarcontainer.style.display = "block";
-//     links.forEach((link) => 
-//     link.addEventListener("click", () => {
-//         navbarlist.classList.remove("show");
-//     })
-// )
-// }
-
 
 function toggleMenu() {
     navbarlist.classList.toggle("show")
 }
 
 function hideMenu() {
-    console.log("hide menu")
     if (navbarlist.classList.contains("show")) {
         navbarlist.classList.remove("show")
     }
