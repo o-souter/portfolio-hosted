bc2c73d3a57b43c604ff0cd19e37df9f4d21a1cf
o-souter <o.souter@rgu.ac.uk>
2023-06-07 16:43:28 +0100

    Added auto dark mode for certain devices, allowing the page to switch to dark mode if it detects the device to be in dark mode

--- javascript/site-scripts.js
+++ javascript/site-scripts.js
@@ -47,79 +47,98 @@ function hideMenu() {
     }
 }
 
-function toggleLightDarkMode() {//Toggle Light/Dark mode function
-    if (mode == 'light') { //Dark mode code
-        mode = 'dark'; 
-        sunMoon.classList.add("fa-regular");
-        sunMoon.classList.remove("fa-solid");
-        html.style.backgroundColor = "#0e0833";
-        for (const el of hyperlinks) {
-            if (el.className != 'nav-link' && el.id != 'hamburger-menu') {el.style.color = "#00ff22"}
-        }
-        introText.style.color = "white";
-        introTitle.style.color = "white";
-        intro.style.boxShadow = "none";
-        moreAboutMe.style.backgroundColor = "#464682";
-        moreAboutMeTitle.style.color = "white";
-        moreAboutMeTxt.style.color = "white";
-        skillsTitle.style.color = "white";
-        for (const el of skillCells) {el.style.color = "white"}
-        languagesTitle.style.color = "white"
-        projects.style.backgroundColor = "#464682"
-        projectsTitle.style.color = "white"
-        for (const el of projectCells) {el.style.backgroundColor = "#545252"}
-        for (const el of projectTitles) {el.style.color = "white"}
-        for (const el of projectDescriptions) {el.style.color = "white"}
-        contactMeTitle.style.color = "white"
-        contactMe.style.boxShadow = "none"
-        for (const el of contactTxt) {el.style.color = "white"}
-        for (const el of contactInputs) {
-            el.style.border = "5px solid white"
-            el.style.backgroundColor = "#0e0833"
-            el.style.color = "white"
-        }
-        findme.style.backgroundColor = "#464682"
-        findme.style.color = "white"
-        for (const el of findmeCells) {el.style.backgroundColor = "#545252"}
-        footer.style.color = "white"
-        footer.style.backgroundColor = "#0e0833"
-    
+//Detecting the device theme when website is accessed to match themes
+function deviceIsDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+if (deviceIsDarkMode()) {
+    setDarkMode();
+}
+//Function to set website theme to dark
+function setDarkMode() {
+    mode = 'dark'; 
+    sunMoon.classList.add("fa-regular");
+    sunMoon.classList.remove("fa-solid");
+    html.style.backgroundColor = "#0e0833";
+    for (const el of hyperlinks) {
+        if (el.className != 'nav-link' && el.id != 'hamburger-menu') {el.style.color = "#00ff22"}
     }
-    
-    else if (mode == 'dark') {//Light Mode code
-        mode = 'light'
-        sunMoon.classList.remove("fa-regular");
-        sunMoon.classList.add("fa-solid");
-        html.style.backgroundColor = "white";
-        for (const el of hyperlinks) {
-            if (el.className != 'nav-link' && el.id != 'hamburger-menu') {el.style.color = "#007910"}
-        }
-        introText.style.color = "black";
-        introTitle.style.color = "black";
-        intro.style.boxShadow = "0px 2px 15px 2px var(--primary-shadow)"
-        moreAboutMe.style.backgroundColor = "#9bd8e9";
-        moreAboutMeTitle.style.color= "black"
-        moreAboutMeTxt.style.color = "black";
-        skillsTitle.style.color = "black";
-        for (const el of skillCells) {el.style.color = "black"}
-        languagesTitle.style.color = "black"
-        projects.style.backgroundColor = "#9bd8e9"
-        projectsTitle.style.color = "black"
-        for (const el of projectCells) {el.style.backgroundColor = "#ebebeb"}
-        for (const el of projectTitles) {el.style.color = "black"}
-        for (const el of projectDescriptions) {el.style.color = "black"}
-        contactMeTitle.style.color = "black"
-        contactMe.style.boxShadow = "0 3px 10px var(--secondary-shadow)"
-        for (const el of contactTxt) {el.style.color = "black"}
-        for (const el of contactInputs) {
-            el.style.border = "5px solid black"
-            el.style.backgroundColor = "white"
-            el.style.color = "black"
-        }
-        findme.style.backgroundColor = "#9bd8e9"
-        findme.style.color = "black"
-        for (const el of findmeCells) {el.style.backgroundColor = "#ebebeb"}
-        footer.style.color = "black"
-        footer.style.backgroundColor = "var(--primary-colour)"
+    introText.style.color = "white";
+    introTitle.style.color = "white";
+    intro.style.boxShadow = "none";
+    moreAboutMe.style.backgroundColor = "#464682";
+    moreAboutMeTitle.style.color = "white";
+    moreAboutMeTxt.style.color = "white";
+    skillsTitle.style.color = "white";
+    for (const el of skillCells) {el.style.color = "white"}
+    languagesTitle.style.color = "white"
+    projects.style.backgroundColor = "#464682"
+    projectsTitle.style.color = "white"
+    for (const el of projectCells) {el.style.backgroundColor = "#545252"}
+    for (const el of projectTitles) {el.style.color = "white"}
+    for (const el of projectDescriptions) {el.style.color = "white"}
+    contactMeTitle.style.color = "white"
+    contactMe.style.boxShadow = "none"
+    for (const el of contactTxt) {el.style.color = "white"}
+    for (const el of contactInputs) {
+        el.style.border = "5px solid white"
+        el.style.backgroundColor = "#0e0833"
+        el.style.color = "white"
+    }
+    findme.style.backgroundColor = "#464682"
+    findme.style.color = "white"
+    for (const el of findmeCells) {el.style.backgroundColor = "#545252"}
+    footer.style.color = "white"
+    footer.style.backgroundColor = "#0e0833"
+}
+
+//Function to set website theme to light
+function setLightMode() {
+    mode = 'light'
+    sunMoon.classList.remove("fa-regular");
+    sunMoon.classList.add("fa-solid");
+    html.style.backgroundColor = "white";
+    for (const el of hyperlinks) {
+        if (el.className != 'nav-link' && el.id != 'hamburger-menu') {el.style.color = "#007910"}
+    }
+    introText.style.color = "black";
+    introTitle.style.color = "black";
+    intro.style.boxShadow = "0px 2px 15px 2px var(--primary-shadow)"
+    moreAboutMe.style.backgroundColor = "#9bd8e9";
+    moreAboutMeTitle.style.color= "black"
+    moreAboutMeTxt.style.color = "black";
+    skillsTitle.style.color = "black";
+    for (const el of skillCells) {el.style.color = "black"}
+    languagesTitle.style.color = "black"
+    projects.style.backgroundColor = "#9bd8e9"
+    projectsTitle.style.color = "black"
+    for (const el of projectCells) {el.style.backgroundColor = "#ebebeb"}
+    for (const el of projectTitles) {el.style.color = "black"}
+    for (const el of projectDescriptions) {el.style.color = "black"}
+    contactMeTitle.style.color = "black"
+    contactMe.style.boxShadow = "0 3px 10px var(--secondary-shadow)"
+    for (const el of contactTxt) {el.style.color = "black"}
+    for (const el of contactInputs) {
+        el.style.border = "5px solid black"
+        el.style.backgroundColor = "white"
+        el.style.color = "black"
+    }
+    findme.style.backgroundColor = "#9bd8e9"
+    findme.style.color = "black"
+    for (const el of findmeCells) {el.style.backgroundColor = "#ebebeb"}
+    footer.style.color = "black"
+    footer.style.backgroundColor = "var(--primary-colour)"
+}
+//Toggle Light/Dark mode function
+function toggleLightDarkMode() {
+    if (mode == 'light') {
+        setDarkMode();
+    }
+    else if (mode == 'dark') {
+        setLightMode();
+    }
+    else {
+        setLightMode();
     }
 }
